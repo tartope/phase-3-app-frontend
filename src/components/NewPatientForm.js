878f9eb34204068545fbcbb91d05c7b8b08b532b
Tunisia Artope <artope@yahoo.com>
2022-03-28 21:49:00 -0400

    updated new patient form

--- src/components/NewPatientForm.js
+++ src/components/NewPatientForm.js
@@ -5,14 +5,18 @@ function NewPatientForm() {
         <>
             <form id="new-patient-form">
                 <input id="patient-name" type="text" placeholder="Patient Name" />
-                <select name="side-effects" id="side-effects" form="new-patient-form">
-                    <option value="dizziness">Dizziness</option>
-                    <option value="nausea">Nausea</option>
-                    <option value="somnambulism">Somnambulism</option>
-                    <option value="memory">Memory</option>
-                    <option value="allergy">Severe Allergic Reaction</option>
-                    <option value="headache">Headache</option>
+                <select name="diagnosis" id="diagnosis" form="new-patient-form">
+                    <option value="stroke">stroke</option>
+                    <option value="knee_replacement">knee replacement</option>
+                    <option value="hip_replacement">hip replacement</option>
+                    <option value="asthma">asthma</option>
+                    <option value="heart_attack">heart attack</option>
+                    <option value="broken_shoulder">broken shoulder</option>
+                    <option value="broken_leg">broken leg</option>
                 </select>
+                <input id="age" type="text" placeholder="Patient Age" />
+                <input id="floor" type="text" placeholder="Floor" />
+                <input id="therapist-name" type="text" placeholder="Therapist Name" />
                 <input type="submit" value="Add Patient" />
             </form>
         </>
