34eabe28d87e1f18239928bac0731a6e2416cd4a
Tunisia Artope <artope@yahoo.com>
2022-03-31 15:38:17 -0400

    header css changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "./Header.css"
 
 function Header({ handleSearch }) {
 
@@ -11,13 +12,30 @@ function Header({ handleSearch }) {
 
     return(
         <header>
-            <div>
-            <img id="headerimg" src="https://dehayf5mhw1h7.cloudfront.net/wp-content/uploads/sites/258/2020/04/29145807/healthcare-symbol.jpg" alt="emrimg"/>
-                <h1>Patient Tracker</h1>
-            </div>
+            <div className="flip-box">
+                <div className="flip-box-inner">
+                    <div className="flip-box-front">
+                        <img id="headerimg" src="https://dehayf5mhw1h7.cloudfront.net/wp-content/uploads/sites/258/2020/04/29145807/healthcare-symbol.jpg" alt="emrimg"/>
+                    </div>
+                    <div className="flip-box-back">
+                        <h2>Patient Tracker</h2>
+                    </div>
+                </div> <h1>Patient Tracker</h1>
+            </div> 
+            
             <input onChange={handleChange} id="search" type="text" placeholder="Search..."></input>
         </header>
     );
+
+    {/* // return(
+    //     <header>
+    //         <div>
+    //         <img id="headerimg" src="https://dehayf5mhw1h7.cloudfront.net/wp-content/uploads/sites/258/2020/04/29145807/healthcare-symbol.jpg" alt="emrimg"/>
+    //             <h1>Patient Tracker</h1>
+    //         </div>
+    //         <input onChange={handleChange} id="search" type="text" placeholder="Search..."></input>
+    //     </header>
+    // ); */}
 }
 
 export default Header;
