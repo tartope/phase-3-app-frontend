806aef15879bd029f4e8afd8ed086ed7cfd2d4c0
Tunisia Artope <artope@yahoo.com>
2022-03-31 15:46:04 -0400

    css animation complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,9 +18,9 @@ function Header({ handleSearch }) {
                         <img id="headerimg" src="https://dehayf5mhw1h7.cloudfront.net/wp-content/uploads/sites/258/2020/04/29145807/healthcare-symbol.jpg" alt="emrimg"/>
                     </div>
                     <div className="flip-box-back">
-                        <h2>Patient Tracker</h2>
+                    <img id="headerimg" src="https://dehayf5mhw1h7.cloudfront.net/wp-content/uploads/sites/258/2020/04/29145807/healthcare-symbol.jpg" alt="emrimg"/>
                     </div>
-                </div> <h1>Patient Tracker</h1>
+                </div> <h1 id="header-patient-tracker">Patient Tracker</h1>
             </div> 
             
             <input onChange={handleChange} id="search" type="text" placeholder="Search..."></input>
